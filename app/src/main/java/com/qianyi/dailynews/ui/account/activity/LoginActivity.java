7357ed024f2ea36c0cbfcf7e266bce7ced40ad7c
xzy031424 <28736834@qq.com>
2018-05-01 23:48:45 +0800

    xzy 更新

---
+++ app/src/main/java/com/qianyi/dailynews/ui/account/activity/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.qianyi.dailynews.ui.account.activity;
+
+import com.qianyi.dailynews.R;
+import com.qianyi.dailynews.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2018/5/1.
+ */
+
+public class LoginActivity extends BaseActivity {
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void getResLayout() {
+        setContentView(R.layout.activity_login);
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void setStatusBarColor() {
+
+    }
+}
