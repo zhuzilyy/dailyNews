219883d81998a3dd8e7119f5201f4be2784b160e
xzy031424 <28736834@qq.com>
2018-05-02 23:31:02 +0800

    xzy 更新

---
+++ app/src/main/java/com/qianyi/dailynews/ui/Mine/activity/TaskCenterActivity.java
@@ -0,0 +1,36 @@
+package com.qianyi.dailynews.ui.Mine.activity;
+
+import com.qianyi.dailynews.R;
+import com.qianyi.dailynews.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2018/5/2.
+ */
+
+public class TaskCenterActivity extends BaseActivity {
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void getResLayout() {
+        setContentView(R.layout.activity_taskcenter);
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void setStatusBarColor() {
+
+    }
+}
