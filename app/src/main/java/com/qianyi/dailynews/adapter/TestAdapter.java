ddeafebe31bc02acf3b8903ad9a04a94bd99d504
zhuzilyy <985085485@email.com>
2018-05-05 14:05:22 +0800

    刷新加载框架

---
+++ app/src/main/java/com/qianyi/dailynews/adapter/TestAdapter.java
@@ -0,0 +1,40 @@
+package com.qianyi.dailynews.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2018/5/5.
+ */
+
+public class TestAdapter extends BaseAdapter {
+    private Context context;
+    public TestAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 20;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        TextView textView=new TextView(context);
+        textView.setText("哈哈哈哈哈");
+        return textView;
+    }
+}
