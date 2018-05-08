285b28f8c1b858149907615f6099dfffadf39545
xzy031424 <287368384@qq.com>
2018-05-08 15:00:02 +0800

    xzy 更新

--- app/src/main/java/com/qianyi/dailynews/adapter/NewsAdapter.java
+++ app/src/main/java/com/qianyi/dailynews/adapter/NewsAdapter.java
@@ -24,6 +24,7 @@ public class NewsAdapter extends BaseAdapter {
 
         for (int i = 1; i <=60 ; i++) {
             if(i%6==1){
+
                 testNewsEntity entity = new NewsAdapter.testNewsEntity("news","0");
                 newsEntities.add(entity);
             }else if(i%6==2){
