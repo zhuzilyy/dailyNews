f9a2c355356372cc979dd726dce72dc63fe0f6e7
xzy031424 <28736834@qq.com>
2018-06-12 21:37:51 +0800

    xzy 6.11

---
+++ app/src/main/java/com/qianyi/dailynews/ui/Mine/adapter/HighRebateAdapter.java
@@ -0,0 +1,51 @@
+package com.qianyi.dailynews.ui.Mine.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.qianyi.dailynews.R;
+
+/**
+ * Created by Administrator on 2018/5/22.
+ */
+
+public class HighRebateAdapter extends BaseAdapter {
+    private Context context;
+    public HighRebateAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 10;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view=LayoutInflater.from(context).inflate(R.layout.item_message,null);
+        return view;
+    }
+}
